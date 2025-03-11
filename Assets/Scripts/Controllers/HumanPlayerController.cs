86fbe6449a1681db77cb9aaa5eb63ad74af8bfaa
ataerena <ataerena599@gmail.com>
2025-03-11 10:57:15 +0300

    code port

--- Assets/Scripts/Controllers/HumanPlayerController.cs
+++ Assets/Scripts/Controllers/HumanPlayerController.cs
@@ -331,6 +331,8 @@ namespace Controllers
                     else if (_humanInput.HorseMount.GetKeyDown() && _human.Horse != null && _human.MountState == HumanMountState.None &&
                     Vector3.Distance(_human.Horse.Cache.Transform.position, _human.Cache.Transform.position) < 15f && !_human.HasDirection)
                         _human.MountHorse();
+                    else if (_humanInput.PassengerMount.GetKeyDown() && _human.MountState == HumanMountState.None)
+                        _human.StartMountingPassengerHorse();
                     else if (_humanInput.Dodge.GetKeyDown())
                     {
                         if (_human.HasDirection)
