cbb5db39450da34bba59668eb3ddaccd3d760816
Momo <64976950+Senssen@users.noreply.github.com>
2025-03-13 19:07:58 +0100

    Shiganshina Gate
    > Custom Shadergraph to make chains fade using alpha cutout when moving up
    > Custom script to get parent world position into Shadergraph
    >Added animation controller with 4 animation states:
    Open
    OpenIdle
    Close
    CloseIdle
    
    Still needs sound and potential particle effect when gate slams down

---
+++ Assets/Resources/ExpeditionAssets/Models/Walls/Materials/GetParentPosition.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GetParentPosition : MonoBehaviour
+{
+    public Material material; // The material that uses the Shader Graph
+
+    void Update()
+    {
+        if (material != null)
+        {
+            Vector3 parentWorldPosition = transform.parent != null ? transform.parent.position : Vector3.zero;
+            material.SetVector("_Parent_World_Position", parentWorldPosition); // Pass to shader
+        }
+    }
+}
