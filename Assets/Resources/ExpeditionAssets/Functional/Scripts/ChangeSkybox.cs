b7ab07851a9ee9eb14bd24773171ebe4e383f035
Momo <64976950+Senssen@users.noreply.github.com>
2025-03-24 18:11:05 +0100

    Added skybox override when launching CityDiorama

---
+++ Assets/Resources/ExpeditionAssets/Functional/Scripts/ChangeSkybox.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkybox : MonoBehaviour
+{
+    private GameObject _gameObject;
+    private Skybox _skybox;
+    public Material _skyboxMaterial;
+    void Awake()
+    {
+        Invoke("UpdateSkybox",2);
+    }
+
+    private void UpdateSkybox()
+    {
+        _gameObject = GameObject.Find("MainCamera(Clone)");
+        _skybox = _gameObject.GetComponent<Skybox>();
+        _skybox.material = _skyboxMaterial;
+
+    }
+
+}
