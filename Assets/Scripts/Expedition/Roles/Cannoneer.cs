328959cee7a9957198b8dab57954d8567d3b2462
ataerena <ataerena599@gmail.com>
2025-05-11 22:48:06 +0300

    half the size of the cannon

--- Assets/Scripts/Expedition/Roles/Cannoneer.cs
+++ Assets/Scripts/Expedition/Roles/Cannoneer.cs
@@ -7,7 +7,7 @@ public class Cannoneer : MonoBehaviour
     public void SpawnCannonController()
     {
         if (Cannon == null) {
-            Vector3 pos = transform.position + (transform.forward * 5f);
+            Vector3 pos = transform.position + (transform.forward * 1f);
             Cannon = PhotonNetwork.Instantiate("Map/Interact/Prefabs/CannoneerCannon", pos, transform.rotation);
         } else {
             Cannon.GetComponent<CannoneerCannon>().UnMount();
